cbf6f23ce5e0a9775d6195b35b1a86cffe50e2d8
kim dongseok(hpc) <pop9846@gmail.com>
2018-09-10 21:25:17 +0900

    drill 03 완성
    2015182005 김동석
    https://youtu.be/29vc33_FiSw

--- drills/drill_03/character_moves.py
+++ drills/drill_03/character_moves.py
@@ -1,10 +1,54 @@
 from pico2d import *
+import math
 
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+x=400
+y=90
+angle=0
+r = 210
+while True:
+    while (30+x < 800):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x + 2
+        delay(0.01)
+    while (45+y < 600):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        y = y + 2
+        delay(0.01)
+    while (x-30 > 0):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        x = x - 2
+        delay(0.01)
+    while (y-90 >0):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        y = y - 2
+        delay(0.01)
+    while (30+x < 430):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x + 2
+        delay(0.01)
+    for i in range(270,630):
+        angle=i*3.14/180
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = r*math.cos(angle)+400
+        y = r*math.sin(angle)+300
+        delay(0.01)
 
 close_canvas()
 
