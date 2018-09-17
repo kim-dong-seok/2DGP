c388e40daeec1b92bf5e83b9921dcc5689686e8f
kim-dong-seok <43131612+kim-dong-seok@users.noreply.github.com>
2018-09-17 16:49:37 +0900

    원 이동 구현
    원 이동 완성

--- drills/drill_05/character_moves.py
+++ drills/drill_05/character_moves.py
@@ -61,15 +61,26 @@ def make_rectangle():
     #move_up()
     #move_left()
     #move_down()
-    move_form_left_to_center()
+    #move_form_left_to_center()
 
+import math
 
 def make_circle():
-    pass
+    cx, cy, r  = 800 // 2, 600 // 2, (600-180) // 2
+    degree = -90
+    while degree < 270:
+        radian = math.radians(degree)
+        x = cx + r * math.cos(radian)
+        y = cy + r * math.sin(radian)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        degree += 1
+        delay(0.01)
 
 
 while True:
-    make_rectangle()
+    #make_rectangle()
     make_circle()
 
     
