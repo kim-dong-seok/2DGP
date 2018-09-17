ac3c0e0227c0ce4903c17514beae7fd7e73ec4fe
kim-dong-seok <43131612+kim-dong-seok@users.noreply.github.com>
2018-09-17 17:29:48 +0900

    이동 2,3,4 구현
    이동 2,3,4 구현

--- drills/drill_05/character_runs.py
+++ drills/drill_05/character_runs.py
@@ -19,15 +19,56 @@ def move_character_01():
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
         update_canvas()
         frame = (frame + 1) % 8
-        x -= (203-132)//15
-        y -= (535-243)//15
+        x -= (203-132)//20
+        y -= (535-243)//20
         delay(0.1)
         get_events()
-
-
-
-move_character_01()
-
-
+def move_character_02():
+    x = 132
+    y = 243
+    frame = 0
+    while x<535:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += (535-132)//20
+        y += (470-243)//20
+        delay(0.1)
+        get_events()
+def move_character_03():
+    x = 535
+    y = 470
+    frame = 0
+    while x>477:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= (535-477)//20
+        y -= (470-203)//20
+        delay(0.1)
+        get_events()
+def move_character_04():
+    x = 477
+    y = 203
+    frame = 0
+    while x<715:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += (715-535)//20
+        y -= (203-136)//20
+        delay(0.1)
+        get_events()
+        
+#move_character_01()
+#move_character_02()
+#move_character_03()
+#move_character_04()
 
 close_canvas()
