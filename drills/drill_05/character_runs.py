c33d20eb88f942290184f0c0d93a7b676f9c1fd7
kim dongseok(hpc) <pop9846@gmail.com>
2018-09-17 22:02:06 +0900

    이동 함수 완성
    이동 함수 완성

--- drills/drill_05/character_runs.py
+++ drills/drill_05/character_runs.py
@@ -1,31 +1,42 @@
 from pico2d import *
+
 open_canvas()
 grass = load_image('grass.png')
-
 character = load_image('animation_sheet.png')
 
 
-
-
-
-
-def move_character(start_x , start_y, last_x, last_ y):
-    x = 203
-    y = 535
+def move_character(start_x , start_y, last_x, last_y):
     frame = 0
-    while x>132:
-        clear_canvas()
-        grass.draw(400, 30)
-        character.clip_draw(frame * 100, 0, 100, 100, x, y)
-        update_canvas()
-        frame = (frame + 1) % 8
-        x -= (203-132)//20
-        y -= (535-243)//20
-        delay(0.1)
-        get_events()
+    x = start_x
+    y = start_y
+    if start_x > last_x:
+        while x > last_x:
+            clear_canvas()
+            grass.draw(400, 30)
+            character.clip_draw(frame * 100, 0, 100, 100, x, y)
+            update_canvas()
+            frame = (frame + 1) % 8
+            x -= (start_x-last_x)//10
+            y -= (start_y-last_y)//10
+            delay(0.5)
+            get_events()
+    elif start_x <= last_x:
+        while x <= last_x:
+            clear_canvas()
+            grass.draw(400, 30)
+            character.clip_draw(frame * 100, 100, 100, 100, x, y)
+            update_canvas()
+            frame = (frame + 1) % 8
+            x -= (start_x-last_x)//10
+            y -= (start_y-last_y)//10
+            delay(0.5)
+            get_events()
 
 
-move_character_01()
+while True:
+    
+    #1
+    move_character(203, 535, 132, 243)
 
 
 close_canvas()
