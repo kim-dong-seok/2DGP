a22a508f1e7da9ada6b1cbf5a0ddf1886611f0c8
kim-young-wan <powerkyw59@naver.com>
2018-09-20 16:48:28 +0900

    캐릭터 그리기
    캐릭터 그리기

--- drills/drill_06/move_character_with_mouse.py
+++ drills/drill_06/move_character_with_mouse.py
@@ -7,6 +7,7 @@ def handle_events():
     global running
     global  m_x, m_y
     global x,y
+    global dir
     events= get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -26,6 +27,7 @@ running = True
 m_x, m_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
+dir=0
 hide_cursor()
 
 while running:
@@ -35,7 +37,8 @@ while running:
 
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
-    frame = (frame + 1) % 8
+    if dir != 0:
+        frame = (frame + 1) % 8
 
     delay(0.02)
     handle_events()
