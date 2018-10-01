8d773141332183acd35d5d43d1e41709cf9d1c67
user(PC11) <>
2018-10-01 17:31:05 +0900

    영상 주소 추가
    2dgp_drills07_2015182005_김동석
    https://youtu.be/6WWwMYCZC-E

--- drills/drill_07/line.py
+++ drills/drill_07/line.py
@@ -2,6 +2,8 @@ from pico2d import *
 import turtle
 import random
 
+
+
 open_canvas()
 grass = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
@@ -31,8 +33,10 @@ size = 20
 points =[(random.randint(0, 500), random.randint(0, 500)) for i in range(size)]
 n = 1
 
+
 while True:
     draw_line(points[n-1], points[n])
     n = (n+1) % size
+
 
 close_canvas()
