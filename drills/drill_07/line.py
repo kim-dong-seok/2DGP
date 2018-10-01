b09377e2aa48b7d2c336e29626d3815a013100b3
user(PC11) <>
2018-10-01 17:21:44 +0900

    캐릭터 좌우 변경
    캐릭터 좌우 변경

--- drills/drill_07/line.py
+++ drills/drill_07/line.py
@@ -9,14 +9,19 @@ character = load_image('animation_sheet.png')
 
 def draw_line(p1, p2):
     frame = 0
+    clip_num =0
     for i in range(0, 100 + 1, 5):
 
         t = i / 100
         x = (1-t)*p1[0]+t*p2[0]
+        if p2[0]-p1[0]>0:
+            clip_num = 100
+        else:
+            clip_num = 0
         y = (1 - t) * p1[1] + t * p2[1]
         clear_canvas()
         grass.draw(500, 500)
-        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        character.clip_draw(frame * 100,clip_num , 100, 100, x, y)
         update_canvas()
         frame = (frame + 1) % 8
         delay(0.05)
