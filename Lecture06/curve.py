4d9609904d0356bba3f89f492931bd9eb2bd3d5d
user(PC11) <>
2018-10-04 16:40:01 +0900

    4점 부드러운 이동 수정 완성
    4점 부드러운 이동 수정 완성

--- Lecture06/curve.py
+++ Lecture06/curve.py
@@ -73,31 +73,14 @@ def draw_curve_4_points(p1, p2, p3, p4):
     draw_big_point(p4)
 
     # draw p1-p2
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p4[0] + (3*t**3 - 5*t**2 + 2)*p1[0] + (-3*t**3 + 4*t**2 + t)*p2[0] + (t**3 - t**2)*p3[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p4[1] + (3*t**3 - 5*t**2 + 2)*p1[1] + (-3*t**3 + 4*t**2 + t)*p2[1] + (t**3 - t**2)*p3[1])/2
-        draw_point((x, y))
-    draw_point(p2)
-    # draw p2-p3
+
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
         y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
         draw_point((x, y))
     draw_point(p3)
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p2[0] + (3*t**3 - 5*t**2 + 2)*p3[0] + (-3*t**3 + 4*t**2 + t)*p4[0] + (t**3 - t**2)*p1[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p2[1] + (3*t**3 - 5*t**2 + 2)*p3[1] + (-3*t**3 + 4*t**2 + t)*p4[1] + (t**3 - t**2)*p1[1])/2
-        draw_point((x, y))
-    draw_point(p4)
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p3[0] + (3*t**3 - 5*t**2 + 2)*p4[0] + (-3*t**3 + 4*t**2 + t)*p1[0] + (t**3 - t**2)*p2[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p3[1] + (3*t**3 - 5*t**2 + 2)*p4[1] + (-3*t**3 + 4*t**2 + t)*p1[1] + (t**3 - t**2)*p2[1])/2
-        draw_point((x, y))
-    draw_point(p1)
+
     # draw p3-p4
 
 
@@ -105,9 +88,13 @@ def draw_curve_4_points(p1, p2, p3, p4):
 
 
 prepare_turtle_canvas()
+points = [(-300, 200), (400, 350), (300, -300), (-200, -200)]
+n = 1
 
-draw_curve_4_points((-300, 200), (400, 350), (300, -300), (-200, -200))
 
+while True:
+    draw_curve_4_points(points[n-3], points[n-2],points[n-1],points[n])
+    n = (n+1) % 4
 
 
 turtle.done()
