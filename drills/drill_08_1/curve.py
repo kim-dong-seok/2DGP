141704d109c3fdc6627f7af5795047b3bfb1744d
user(PC11) <>
2018-10-04 16:54:32 +0900

    20개 점 이동 함수수정
    20개 점 이동 함수수정

--- drills/drill_08_1/curve.py
+++ drills/drill_08_1/curve.py
@@ -7,19 +7,18 @@ open_canvas()
 grass = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
-
-def draw_line(p1, p2):
+def draw_random_soft_move(p1, p2,p3,p4):
     frame = 0
     clip_num =0
-    for i in range(0, 100 + 1, 5):
+    for i in range(0, 100 , 5):
 
         t = i / 100
-        x = (1-t)*p1[0]+t*p2[0]
-        if p2[0]-p1[0]>0:
+        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        if p3[0]-p2[0]>0:
             clip_num = 100
         else:
             clip_num = 0
-        y = (1 - t) * p1[1] + t * p2[1]
+        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
         clear_canvas()
         grass.draw(500, 500)
         character.clip_draw(frame * 100,clip_num , 100, 100, x, y)
@@ -34,8 +33,8 @@ n = 1
 
 
 while True:
-    draw_line(points[n-1], points[n])
-    n = (n+1) % size
+    draw_random_soft_move(points[n - 3], points[n - 2], points[n - 1], points[n])
+    n = (n + 1) % size
 
 
 close_canvas()
