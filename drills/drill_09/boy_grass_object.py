106397d9bd951fb4d9e2c4b032379c9c7a5ac42c
user(PC11) <>
2018-10-08 16:53:34 +0900

    랜덤공 클래스 생성
    랜덤공 클래스 생성

---
+++ drills/drill_09/boy_grass_object.py
@@ -0,0 +1,57 @@
+from pico2d import *
+from random import *
+
+# Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400,30)
+
+class Boy:
+
+    def __init__(self):
+        self.x = randrange(0,600,30)
+        self.y = 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+class randomBall:
+    pass
+
+def handle_events():
+
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+# initialization code
+open_canvas()
+team = [Boy() for i in range(11)]
+grass = Grass()
+running = True;
+# game main loop code
+while running:
+    handle_events()
+    for boy in team:
+        boy.update()
+    clear_canvas()
+    grass.draw()
+    for boy in team:
+        boy.draw()
+    update_canvas()
+    delay(0.05)
+# finalization code
+close_canvas()
